009663ff3350ac1190cc57dcd23690bbc2342530
frankhn <harfrank2@gmail.com>
2020-08-10 07:32:33 +0200

    connects the component to store

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import ReactFlagsSelect from 'react-flags-select';
-import { Row, Col, InputGroup, Input, InputGroupAddon, InputGroupText } from 'reactstrap'
+import React, { Component } from 'react';
 import 'react-flags-select/css/react-flags-select.css';
 import './App.css'
 import Form from './resources/form'
+import request from './resources/config/Axios'
+import { connect } from 'react-redux';
+import { AppState } from './store';
+import { checkRates } from './store/actions'
+import Spinner from './resources/spinner'
 
 interface Props {
+  rates: any
+  loading: boolean
+  checkRates:any
+}
 
+interface State {
+  amountA: number
+  amountB: number
+  countryB: string
+  currentExchangeRate: number
+  rates: any
+  baseCurrency: string,
+  date: Date
+  error: string
 }
 
 
-const App = (props: Props) => {
-  const [ state, setState ] = useState({
-    amountA: 1,
-    amountB: 2,
-    countryA: "GB",
-    countryB: "US",
-    currentExchangeRate: null
-  })
+class App extends Component<Props, State> {
+  // const [ state, setState ] = useState(initial)
 
-  const handleAmountChange = (e: any) => {
-    setState({ ...state, [ e.target.name ]: e.target.value })
+  state = {
+    amountA: 1,
+    amountB: 1,
+    countryB: "RUB",
+    currentExchangeRate: 0,
+    rates: {},
+    baseCurrency: "USD",
+    date: new Date(),
+    error: ""
   }
 
-  useEffect(() => {
-    console.log("State changed")
-  })
+  handleChange = (e: any) => {
+    this.setState({ amountA: e.target.value })
+  }
 
-  return (
-    <div className="container"
-      style={ { margin: "20vh auto", padding: "2rem 3rem" } }>
-      <Form change={handleAmountChange} initial={state} />
-    </div>
-  );
+  handleCountryChange = (countryCode: string) => {
+    this.setState({ countryB: countryCode })
+  }
+
+  handleBaseCountryChange = (countryCode: string) => {
+    this.setState({ baseCurrency: countryCode })
+    this.props.checkRates(countryCode)
+  }
+
+  componentDidMount() {
+    this.props.checkRates("USD")
+  }
+
+
+  render() {
+    const { rates, loading } = this.props
+    let amountB = 1
+    if(rates !== undefined) {
+      const rate = rates[this.state.countryB]
+      amountB = rate * this.state.amountA
+    }
+    return (
+      <div className="container"
+        style={{ margin: "20vh auto", padding: "2rem 3rem" }}>
+        <p>{this.state.error}</p>
+        {
+          loading 
+          ? <Spinner />
+          : rates && rates
+            ? <Form
+                change={this.handleChange}
+                initial={{...this.state, amountB }}
+                baseCountry={this.handleBaseCountryChange}
+                countryChange={this.handleCountryChange} />
+          : <Spinner />
+        }
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state: AppState) => {
+ const {rates, loading, error } = state.Rates
+ return {
+   rates, loading, error
+ }
+}
+
+export default connect(mapStateToProps, {checkRates})(App)
